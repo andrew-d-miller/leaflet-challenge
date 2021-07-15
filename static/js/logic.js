2e746761a445cab33f8dd7908c35151c59250736
Andrew Miller <amiller9108@gmail.com>
2021-07-15 12:25:58 -0400

    code push, still needs to be tweaked

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,7 @@
 // Store our API endpoint inside queryUrl
-var quakeUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var quakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+var tectonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
 // Perform a GET request to the query URL
 d3.json(quakeUrl).then(function(data) {
